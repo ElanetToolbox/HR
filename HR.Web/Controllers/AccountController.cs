3fd04e46be903e12bec677597f75ddd5a3d790b5
Jason <chatziparadeisis.i@elanet.gr>
2020-09-21 16:17:14 +0300

    login page hooked

---
+++ HR.Web/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using HR.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            if (acc.Username == "1" && acc.Password == "1")
+            {
+                return View("/Employees");
+            }
+            else
+            {
+                return View("");
+            }
+        }
+    }
+}
